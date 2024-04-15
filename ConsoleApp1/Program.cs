db648da1f58c0e9a62dc29cd4bc2b156a05ffa3a
Алла <machkarina@gmail.com>
2024-04-15 21:41:23 +0300

    метод для вывода массива

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -29,5 +29,13 @@
             }
             return resultArray;
         }
+
+        static void PrintArray(string[] array)
+        {
+            foreach (string str in array)
+            {
+                Console.WriteLine(str);
+            }
+        }
     }
 }
