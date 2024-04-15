e02baf8fd2859c888cd96e4ed7e84b0b3d85792a
Алла <machkarina@gmail.com>
2024-04-15 21:38:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = { "apple", "banana", "cat", "dog", "elephant", "fo", "goat" };
+
+            
+        }
+    }
+}
